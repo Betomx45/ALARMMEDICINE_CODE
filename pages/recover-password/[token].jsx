6b53cfbebf7aa09a50085d816795e53c2058fa4f
Luis Alberto <92551372+Betomx45@users.noreply.github.com>
2023-07-04 13:25:27 -0600

    Corrección de errores

--- pages/recover-password/[token].jsx
+++ pages/recover-password/[token].jsx
@@ -1,114 +1,177 @@
-import apiCLient from '@/apiClient';
-import { Button, Container, Paper, TextField, Typography } from '@mui/material';
-import db from  'database/models';
-import Head from 'next/head';
-import { useState } from 'react';
-import { Op } from 'sequelize';
+import apiClient from "@/apiClient";
+import { Button, Grid, Paper, TextField, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { useState } from "react";
+import db from "database/models/index";
+import { Op } from "sequelize";
+import Head from "next/head";
 
 const RecoverPassword = (props) => {
-    const { token } = props;
-    const [ password, setPassword ] = useState('');
-    const [ message, setMessage ] = useState(props.message);
-    const [ manifest, setManifest ] = useState(props.token ? 'from' : 'result');
+  const { token } = props;
+  const [mensaje, setMensaje] = useState(props.message);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [mostrar, setMostrar] = useState(props.token ? "form" : "result");
+  const [showPwd, setShowPwd] = useState(false)
 
-    const handleChange = (e) => {
-        setPassword(e.target.value);
+  const handleChange = (e) => {
+    setPassword(e.target.value);
+    if (confirmPassword && e.target.value !== confirmPassword) {
+      setPasswordError("Las contraseñas no coinciden");
+    } else {
+      setPasswordError("");
     }
+  };
 
-    const handleRecovery = (e) => {
-        e.preventDefault();
+  const handleConfirmChange = (e) => {
+    setConfirmPassword(e.target.value);
+    if (password && e.target.value !== password) {
+      setPasswordError("Las contraseñas no coinciden");
+    } else {
+      setPasswordError("");
+    }
+  };
 
-        apiCLient.post('/api/password/change', { password, token })
-        .then((response) => {
-            console.log(response.data);
-            setManifest('result');
-            setMessage(response.data.message);
-        })
-        .catch((error) => {
-            console.log(error);
-            setManifest('result')
-            setMessage(error.message || 'Error al intentar guardar la nueva contrasena');
-        }); 
-    };
+  const handleRecovery = (e) => {
+    e.preventDefault();
 
-    const renderContent = () => {
-        if( manifest === 'from' ) {
-            return (
-                <form onSubmit={handleRecovery} noValidate>
-                    <TextField
-                        margin='normal'
-                        variant='outlined'
-                        required
-                        fullWidth
-                        name='Password'
-                        label='Ingresa nueva contrasena'
-                        type='passwors'
-                        id='password'
-                        value={password}
-                        onChange={handleChange}
-                    />
-                    <Button
-                        type='submit'
-                        fullWidth
-                        variant='contained'
-                        color='primary'
-                    >
-                        Restablecer Contrasena
-                    </Button>
-                </form>
-            );
-        }
+    //realizar envío de nueva contraseña
+    apiClient.post("/password/change", { password, token })
+      .then((response) => {
+        console.log(response.data);
+        setMostrar("result");
+        setMensaje(response.data.message);
+      })
+      .catch((error) => {
+        console.log(error);
+        setMostrar("result");
+        setMensaje(error.message || "Error al intentar guardar la nueva contraseña.");
+      });
+  };
 
-        return (
-            <Typography variant='h4'>{message}</Typography>
-        )
+  const renderContent = () => {
+
+    if (mostrar === "form") {
+      return (
+        //Formulario
+        <form onSubmit={handleRecovery} noValidate>
+          <Grid container spacing={4} sx={{ display: "flex", justifyContent: "center" }} >
+            <Grid item xs={12} md={8} sx={{ my: 3 }}>
+              <Typography
+                color="primary"
+                variant="h5"
+                mt={6}
+                sx={{ fontWeight: "bold" }}
+              >
+                Ingresa la nueva contraseña
+              </Typography>
+              <TextField
+                margin="normal"
+                variant="standard"
+                required
+                fullWidth
+                name="password"
+                type={showPwd ? "text" : "password"}
+                id="password"
+                value={password}
+                onChange={handleChange}
+                error={!!passwordError}
+                helperText={passwordError}
+              />
+              <Typography
+                color="primary"
+                variant="h5"
+                mt={6}
+                sx={{ fontWeight: "bold" }}
+              >
+                Confirmar contraseña
+              </Typography>
+              <TextField
+                margin="normal"
+                variant="standard"
+                required
+                fullWidth
+                name="password"
+                type="password"
+                id="password"
+                value={confirmPassword}
+                onChange={handleConfirmChange}
+                error={!!passwordError}
+                helperText={passwordError}
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              sx={{ fontSize: "20px" }}
+              fullWidth
+              variant="contained"
+            >
+              Restablecer contraseña
+            </Button>
+          </Grid>
+        </form>
+      );
     }
 
     return (
-        <Container>
-            <Head>
-                <title>Recuperar contrasena</title>
-            </Head>
-            <Paper>
-                <Typography variant='h6'>Recuperar contrasena</Typography>
-                {renderContent()}
-            </Paper>
-        </Container>
+      <Typography sx={{ marginTop: 8, textAlign: 'center' }} variant="h6">{mensaje}</Typography>
     );
+  };
+
+  return (
+    //Renderizado
+    <Container>
+      <Head>
+        <title>Recuperar Contraseña.</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossorigin="anonymous"
+        />
+      </Head>=
+      <Navbar className="menu" fixed="top">
+        <Navbar.Brand id="unidadesTitle">Recuperar Contraseña</Navbar.Brand>
+      </Navbar>
+      {renderContent()}
+    </Container>
+  );
 };
 
+export async function getServerSideProps({ req, res, params }) {
+  // Leer el token
+  const { token } = params;
+  console.log(token);
 
+  //Buscar el usuario mediante el token de recuperacion
+  const user = await db.Usuario.findOne({
+    where: {
+      passwordResetToken: token,
+      passwordResetExpire: { [Op.gt]: new Date() },
+    },
+  });
 
-    export async function getServerSideProps ({ req, res, params }) {
-        //leer token
-        const { token } = params;
-        console.log(token);
+  if (!user) {
+    return {
+      props: {
+        token: null,
+        message:
+          "El link de recuperacion de contraseña no es invalido o ha expirado",
+      },
+    };
+  }
 
-        //buscar el usuario con este token de recuperacion
-        const user = await db.Usuario.findOne(
-            {
-                where: {
-                    paswordResetToken: token,
-                    passwordResetExpire: {[ Op.gt ]: new Date()},
-                }
-            }
-        );
-
-        if( !user ) {
-            return {
-                props: {
-                    token: null,
-                    message: 'El link de recuperacion ha expirado'
-                },
-            }
-        }
-
-        return {
-            props: {
-                token,
-                message: 'Ingresar la nueva contrasena',
-            },
-        }
-    }
+  return {
+    props: {
+      token,
+      message: "Ingresar la nueva contraseña",
+    },
+  };
+}
 
 export default RecoverPassword;
